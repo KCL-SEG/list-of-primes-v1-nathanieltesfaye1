61045bd7a2e1b982cfbd9657b596a1ee5b8227cc
Nathaniel Tesfaye <nathaniel.tesfaye@kcl.ac.uk>
2022-09-30 14:07:24 +0100

    Add functionality to print certain number of prime number

--- primes.py
+++ primes.py
@@ -3,4 +3,16 @@
 
 def primes(number_of_primes):
     list = []
+
+    number = 2
+
+    while len(list) != number_of_primes:
+        for i in range(2, number):
+            if number % i == 0:
+                break
+        else:
+            list.append(number)
+
+        number += 1
+
     return list
